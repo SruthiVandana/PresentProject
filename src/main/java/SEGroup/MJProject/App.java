60cec927ee82ba367f4008efc9c3214439bdbb46
sruthi <vsv252004@gmail.com>
2024-11-14 21:10:35 +0530

    First commit to maven java project

---
+++ src/main/java/SEGroup/MJProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.MJProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!. I am V. Sruthi Vandana - 22BD1A665X " );
+    }
+}
