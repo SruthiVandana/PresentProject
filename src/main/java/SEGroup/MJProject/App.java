1d8f46664a965f4e3a732a9ac5c29b382adc1a0d
sruthi <vsv252004@gmail.com>
2024-11-22 13:02:34 +0530

    Demo Java

--- src/main/java/SEGroup/MJProject/App.java
+++ src/main/java/SEGroup/MJProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!. I am V. Sruthi Vandana - 22BD1A665X " );
+        System.out.println( "Hello World!. I am V.Sruthi Vandhana - 22BD1A665X " );
     }
 }
