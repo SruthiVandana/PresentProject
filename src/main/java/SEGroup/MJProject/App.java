b8a562755eb9b9b172a4b551b226a8ec617d16c5
SruthiVandana <sruthivandana25@gmail.com>
2024-11-30 10:27:40 +0530

    commit 1

--- src/main/java/SEGroup/MJProject/App.java
+++ src/main/java/SEGroup/MJProject/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!. I am V.Sruthi Vandhana - 22BD1A665X " );
+        System.out.println("Demo Java" ); 
     }
 }
